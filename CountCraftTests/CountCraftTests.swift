70885cfa6e24846c947e46cf4fc57d198283eca1
Mike Schwab <mike.schwab@gmail.com>
2026-01-16 10:00:48 -0500

    Initial Commit

---
+++ CountCraftTests/CountCraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountCraftTests.swift
+//  CountCraftTests
+//
+//  Created by Mike Schwab on 1/16/26.
+//
+
+import Testing
+@testable import CountCraft
+
+struct CountCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
